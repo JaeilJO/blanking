daec2edc9b32d272247d4a3a8103ce51490a7419
JaeilJo <whwodlf97@gmail.com>
2024-01-06 02:27:08 +0900

    feat : Auth Slide 동작 확인

--- src/app/(unauthrized)/layout.tsx
+++ src/app/(unauthrized)/layout.tsx
@@ -1,9 +1,12 @@
+import AuthSlide from '@/components/AuthSlide';
 import style from './layout.module.scss';
 
 function Layout({ children }: { children: React.ReactNode }) {
     return (
         <div className={style.background_wrapper}>
-            <div className={style.slide_intro_wrapper}>slide</div>
+            <div className={style.slide_intro_wrapper}>
+                <AuthSlide />
+            </div>
             <div className={style.auth_wrapper}>{children}</div>
         </div>
     );
---
+++ src/components/AuthSlide/index.tsx
@@ -0,0 +1,12 @@
+import style from './index.module.scss';
+function AuthSlide() {
+    return (
+        <ul className={style.ul}>
+            <li className={style.li}>Hello</li>
+            <li className={style.li}>Hello</li>
+            <li className={style.li}>Hello</li>
+        </ul>
+    );
+}
+
+export default AuthSlide;
