cde36c670b644a0f062f8bebe7c34df79b55f214
JaeilJo <whwodlf97@gmail.com>
2024-01-05 21:52:48 +0900

    feat : Submit button 제작

--- src/components/AuthComponent/AuthCommon/Form/SubmitButton/index.tsx
+++ src/components/AuthComponent/AuthCommon/Form/SubmitButton/index.tsx
@@ -1,5 +1,13 @@
-function SubmitButton() {
-    return <div>submit</div>;
+import classNames from 'classnames/bind';
+import style from './index.module.scss';
+
+interface SubmitButtonProps {
+    value: string;
+    disabeld?: boolean;
+}
+
+function SubmitButton({ value, disabeld = false }: SubmitButtonProps) {
+    return <input type="submit" className={style.input_button} disabled={disabeld} value={value} />;
 }
 
 export default SubmitButton;
