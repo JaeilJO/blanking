990f230d19015e2596e2658dcc2f9b3bbd70e76a
JaeilJo <whwodlf97@gmail.com>
2024-01-05 15:30:55 +0900

    feat : unauthrized layout style 설정

--- src/app/(unauthrized)/layout.tsx
+++ src/app/(unauthrized)/layout.tsx
@@ -1,8 +1,10 @@
+import style from './layout.module.scss';
+
 function Layout({ children }: { children: React.ReactNode }) {
     return (
-        <div>
-            unauthrized layout
-            {children}
+        <div className={style.background_wrapper}>
+            <div className={style.slide_intro_wrapper}>slide</div>
+            <div className={style.auth_wrapper}>{children}</div>
         </div>
     );
 }
