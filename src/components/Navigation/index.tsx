f87c83b79b0521351c0cea7a3e757fd143b5ef3c
JaeilJo <whwodlf97@gmail.com>
2024-01-06 14:45:29 +0900

    feat : logout 버튼 추가

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import GroupCategory from './GroupCategory';
 import style from './index.module.scss';
+import { BsDoorOpen } from 'react-icons/bs';
 
 function Navigation({ username, groups }: { username: string; groups: any }) {
     return (
@@ -9,7 +10,20 @@ function Navigation({ username, groups }: { username: string; groups: any }) {
                 {username}
             </Link>
             <hr className={style.line} />
-            {groups.length === 0 ? <div>Group이 없습니다.</div> : <GroupCategory groups={groups} username={username} />}
+            <div className={style.category_wrapper}>
+                {groups.length === 0 ? (
+                    <div>Group이 없습니다.</div>
+                ) : (
+                    <GroupCategory groups={groups} username={username} />
+                )}
+            </div>
+            <button className={style.logout_button}>
+                <span className={style.logout_icon}>
+                    <BsDoorOpen />
+                </span>
+
+                <span>Logout</span>
+            </button>
         </div>
     );
 }
