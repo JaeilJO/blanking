07366d7cbff4faab9265e9bff12ece658c478586
JaeilJo <whwodlf97@gmail.com>
2024-01-07 01:06:33 +0900

    feat : Page Title 스타일적용

--- src/app/(authorized)/[username]/[group]/[page]/page.tsx
+++ src/app/(authorized)/[username]/[group]/[page]/page.tsx
@@ -1,16 +1,19 @@
 import EditorJs from '@/components/EditorJs';
 import { basic_mock } from '@/mock/basic';
+import style from './page.module.scss';
 
 function Page({ params }: { params: { page: string } }) {
     const user = basic_mock;
-    console.log(params);
+
+    const page_name = decodeURIComponent(params.page as string);
+
     const group = user.groups.filter((group) => group.groupname === params.group);
 
-    const page = group[0]?.pages?.filter((page) => page.pagename === params.page)[0];
-    console.log(page?.content);
+    const page = group[0]?.pages?.filter((page) => page.pagename === page_name)[0];
+
     return (
-        <div>
-            {params.page}
+        <div className={style.page_wrapper}>
+            <div className={style.page_title}>{page_name}</div>
 
             <EditorJs data={page?.content} />
         </div>
