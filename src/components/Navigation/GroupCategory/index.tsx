a820f026249f6ca5e856fe51207412dfb924fa1c
JaeilJo <whwodlf97@gmail.com>
2024-01-06 12:30:39 +0900

    feat : Group Category 현재 페이지에 대한 반응형 스타일 추가

--- src/components/Navigation/GroupCategory/index.tsx
+++ src/components/Navigation/GroupCategory/index.tsx
@@ -4,6 +4,8 @@ import Link from 'next/link';
 import style from './index.module.scss';
 import { useParams } from 'next/navigation';
 import classNames from 'classnames/bind';
+import PageCategory from '../PageCategory';
+import { BsFillCaretLeftFill } from 'react-icons/bs';
 
 interface GroupCategoryProps {
     groups: any;
@@ -18,28 +20,32 @@ function GroupCategory({ groups, username }: GroupCategoryProps) {
     return (
         <ul className={style.group_category_wrapper}>
             {groups.map((group: any) => (
-                <li key={group.groupName}>
-                    <Link
-                        href={`/${username}/${group.groupName}`}
-                        className={cn({
-                            group_title: true,
-                            current: params.group === group.groupName,
-                        })}
-                    >
-                        {group.groupName}
-                    </Link>
+                <li key={group.groupname}>
+                    <div className={style.group_title_wrapper}>
+                        <Link
+                            href={`/${username}/${group.groupname}`}
+                            className={cn({
+                                group_title: true,
+                                current: params.group === group.groupname,
+                            })}
+                        >
+                            {group.groupname}
+                        </Link>
+                        <span
+                            className={cn({
+                                group_title_icon: true,
+                                current: params.group === group.groupname,
+                            })}
+                        >
+                            <BsFillCaretLeftFill />
+                        </span>
+                    </div>
 
                     {/* Page Category */}
                     {group.pages === undefined ? (
                         <div>페이지가 없습니다</div>
                     ) : (
-                        <ul>
-                            {group.pages.map((page: any) => (
-                                <li key={page.pagename}>
-                                    <h3>{page.pagename}</h3>
-                                </li>
-                            ))}
-                        </ul>
+                        <PageCategory username={username} groupname={group.groupname} pages={group.pages} />
                     )}
                 </li>
             ))}
