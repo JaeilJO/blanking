012eced6d8bfb2eec85a5112d0f3ff04d1c1c4e2
JaeilJo <whwodlf97@gmail.com>
2024-01-06 22:25:04 +0900

    refactor : navigation의 position을 fixed로 변경

--- src/components/PageTable/index.tsx
+++ src/components/PageTable/index.tsx
@@ -8,7 +8,9 @@ import { useParams } from 'next/navigation';
 
 function PageTable({ pages }: { pages: any }) {
     const param = useParams();
+
     console.log(param);
+
     return (
         <ul className={style.page_contatiner}>
             {pages?.map((page: any) => (
