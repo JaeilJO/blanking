e33a424e58f1ac816fcaa43f38a438354f1d2149
JaeilJo <whwodlf97@gmail.com>
2024-01-06 17:48:20 +0900

    feat : [username] 라우터에 redircet 설정
    - groups를 확인하고 있다면 groups의 첫번째로 redirect 되게 설정

--- src/app/(authorized)/[username]/page.tsx
+++ src/app/(authorized)/[username]/page.tsx
@@ -1,15 +1,22 @@
+import { redirect } from 'next/navigation';
+
 import { basic_mock } from '@/mock/basic';
 import style from './page.module.scss';
 import NoGroup from '@/components/NoGroup';
+import GroupTable from '@/components/GroupTable';
 
-export default function Page({ params }: { params: { username: string } }) {
+export default async function Page({ params }: { params: { username: string } }) {
     const user = basic_mock;
 
-    if (user.groups === undefined) return <div>페이지가 없습니다</div>;
+    if (user.groups) {
+        redirect(`${user.name}/${user.groups[0].groupname}`);
+    }
 
-    return (
-        <div className={style.userpage_wrapper}>
-            <NoGroup />
-        </div>
-    );
+    if (user.groups === undefined) {
+        return (
+            <div className={style.userpage_wrapper}>
+                <NoGroup />
+            </div>
+        );
+    }
 }
---
+++ src/components/GroupTable/index.tsx
@@ -0,0 +1,11 @@
+import style from './index.module.scss';
+
+function GroupTable({ groups }: { groups: any }) {
+    return (
+        <div className={style.group_container}>
+            <div>Hello</div>
+        </div>
+    );
+}
+
+export default GroupTable;
