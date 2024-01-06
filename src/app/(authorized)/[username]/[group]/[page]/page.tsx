bdbc7065497fae4d0cebdb16b3f88ebb141997fc
JaeilJo <whwodlf97@gmail.com>
2024-01-07 00:43:13 +0900

    feat : Editor js 적용

--- src/app/(authorized)/[username]/[group]/[page]/page.tsx
+++ src/app/(authorized)/[username]/[group]/[page]/page.tsx
@@ -1,5 +1,20 @@
+import EditorJs from '@/components/EditorJs';
+import { basic_mock } from '@/mock/basic';
+
 function Page({ params }: { params: { page: string } }) {
-    return <div>{params.page}</div>;
+    const user = basic_mock;
+    console.log(params);
+    const group = user.groups.filter((group) => group.groupname === params.group);
+
+    const page = group[0]?.pages?.filter((page) => page.pagename === params.page)[0];
+    console.log(page?.content);
+    return (
+        <div>
+            {params.page}
+
+            <EditorJs data={page?.content} />
+        </div>
+    );
 }
 
 export default Page;
--- src/components/EditorJs/index.tsx
+++ src/components/EditorJs/index.tsx
@@ -1,5 +1,34 @@
-function EditorJs() {
-    return <></>;
+'use client';
+
+import EditorJS from '@editorjs/editorjs';
+
+import { useEffect, useRef } from 'react';
+
+import style from './index.module.scss';
+import { EDITOR_JS_TOOLS } from '@/utils/editorjstools';
+
+function EditorJs({ data }: { data?: any }) {
+    const ref = useRef(null);
+
+    useEffect(() => {
+        if (!ref.current) {
+            const editor = new EditorJS({
+                holder: 'editor',
+                data,
+                tools: EDITOR_JS_TOOLS,
+            });
+
+            ref.current = editor;
+        }
+
+        return () => {
+            if (ref.current && ref.current.destroy) {
+                ref.current.destroy();
+            }
+        };
+    }, []);
+
+    return <div id="editor" className={style.editor} />;
 }
 
 export default EditorJs;
