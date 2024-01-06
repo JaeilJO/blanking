5731ae99dae2d3fd00d9cfad531509a12cf4306d
JaeilJo <whwodlf97@gmail.com>
2024-01-06 16:21:04 +0900

    feat: No Group페이지 완성

--- src/components/NoGroup/index.tsx
+++ src/components/NoGroup/index.tsx
@@ -7,7 +7,9 @@ function NoGroup() {
             <div className={style.nogroup_content_wrapper}>
                 <h1 className={style.title}>Make Group</h1>
                 <GroupMaker />
-                <div>특수문자를 제외한 영어 대 소문자 혹은 한글만을 사용해주세요</div>
+                <div className={style.subscribe}>
+                    특수문자를 제외한 <b>영어 대 소문자 혹은 한글</b>만을 사용해주세요
+                </div>
             </div>
         </div>
     );
