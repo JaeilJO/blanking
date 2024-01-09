e942ba1489b40f2f15ff483ba7d68edf27a8620f
JaeilJo <whwodlf97@gmail.com>
2024-01-09 13:06:46 +0900

    fix : signup signin Link태그 주소 변경

--- src/components/AuthComponent/AuthCommon/AccountInquiry/index.tsx
+++ src/components/AuthComponent/AuthCommon/AccountInquiry/index.tsx
@@ -5,7 +5,7 @@ function AccountInquery({ authType }: { authType: 'signin' | 'signup' }) {
     return (
         <div className={style.account_inquiry_wrapper}>
             <span>{authType === 'signin' ? '계정이 이미 있으신가요?' : '계정이 없으신가요?'}</span>
-            <Link className={style.link_text} href={authType === 'signin' ? '/signup' : '/'}>
+            <Link className={style.link_text} href={authType === 'signin' ? '/auth/signup' : '/auth/signin'}>
                 {authType === 'signin' ? 'Sign Up' : 'Sign In'}
             </Link>
         </div>
