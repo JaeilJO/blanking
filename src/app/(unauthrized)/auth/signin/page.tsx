630b66ec40ee7224c6954821df31fc3f1e81ee11
JaeilJo <whwodlf97@gmail.com>
2024-01-09 15:09:41 +0900

    feat : getServerSession을 위해서 env 설정

--- src/app/(unauthrized)/auth/signin/page.tsx
+++ src/app/(unauthrized)/auth/signin/page.tsx
@@ -1,6 +1,9 @@
 import SignInComponent from '@/components/AuthComponent/SignInComponent';
+import { config } from '@/utils/auth';
+import { cookies, headers } from 'next/headers';
+import { getServerSession as originalGetServerSession } from 'next-auth';
 
-function Page() {
+async function Page() {
     return (
         <>
             <SignInComponent />
--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -9,25 +9,24 @@ export const config = {
 
             credentials: {},
             async authorize(credentials, req) {
-                console.log('hello');
+                console.log('here is Next Auth authorize');
                 return { id: '1', name: 'test', email: 'test@test.com' };
             },
         }),
     ],
     callbacks: {
-        async signIn({ user, account, profile, email, credentials }) {
-            console.log('hello Signin', user);
-            return true;
-        },
-        async session({ session, token, user }) {
-            console.log('session', session);
-            return session;
-        },
-
-        async jwt({ token, user, account, profile, isNewUser }) {
-            console.log('toekn', token);
-            return token;
-        },
+        // async signIn({ user, account, profile, email, credentials }) {
+        //     console.log('hello Signin', user);
+        //     return true;
+        // },
+        // async session({ session, token, user }) {
+        //     console.log('session', session);
+        //     return session;
+        // },
+        // async jwt({ token, user, account, profile, isNewUser }) {
+        //     console.log('toekn', token);
+        //     return token;
+        // },
     },
     pages: {
         signIn: '/auth/signin',
