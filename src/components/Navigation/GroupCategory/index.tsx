897073b0aa30601730ae27599bfe6b126ad1342c
JaeilJo <whwodlf97@gmail.com>
2024-01-11 21:47:46 +0900

    feat : Group title icon 변경

--- src/components/Navigation/GroupCategory/index.tsx
+++ src/components/Navigation/GroupCategory/index.tsx
@@ -5,8 +5,10 @@ import style from './index.module.scss';
 import { useParams } from 'next/navigation';
 import classNames from 'classnames/bind';
 import PageCategory from '../PageCategory';
-import { BsFillCaretLeftFill } from 'react-icons/bs';
+
+import { BsFolder2Open } from 'react-icons/bs';
 import { BsFillPlusSquareFill } from 'react-icons/bs';
+import { BsFolder } from 'react-icons/bs';
 interface GroupCategoryProps {
     groups: any;
     username: string;
@@ -24,6 +26,14 @@ function GroupCategory({ groups, username }: GroupCategoryProps) {
             {groups.map((group: any) => (
                 <li key={group.groupname}>
                     <div className={style.group_title_wrapper}>
+                        <span
+                            className={cn({
+                                group_title_icon: true,
+                                current: current_group_name === group.groupname,
+                            })}
+                        >
+                            {current_group_name === group.groupname ? <BsFolder2Open /> : <BsFolder />}
+                        </span>
                         <Link
                             href={`/user/${username}/${group.groupname}`}
                             className={cn({
@@ -33,14 +43,6 @@ function GroupCategory({ groups, username }: GroupCategoryProps) {
                         >
                             {group.groupname}
                         </Link>
-                        <span
-                            className={cn({
-                                group_title_icon: true,
-                                current: current_group_name === group.groupname,
-                            })}
-                        >
-                            <BsFillCaretLeftFill />
-                        </span>
                     </div>
 
                     {/* Page Category */}
@@ -51,6 +53,8 @@ function GroupCategory({ groups, username }: GroupCategoryProps) {
                     )}
                 </li>
             ))}
+
+            {/* // Group 생성 버튼 */}
             <li>
                 <Link className={style.create_group_button} href={`/user/creategroup`}>
                     <span className={style.create_group_button_icon}>
