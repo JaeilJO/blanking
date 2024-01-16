321a1522c2ac81a9481f2abfc407563c102ae5da
JaeilJo <whwodlf97@gmail.com>
2024-01-16 16:08:59 +0900

    feat : getGroups axios로 변경

--- src/lib/getGroups.ts
+++ src/lib/getGroups.ts
@@ -1,22 +1,5 @@
-interface getGroupsArguemnts {
-    userid: string;
-}
+import axios from 'axios';
 
-export default async function getGroups({ userid }: getGroupsArguemnts) {
-    console.log('userid', userid);
-    const res = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api/group/${userid}`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        next: {
-            tags: ['groups'],
-        },
-    });
-
-    if (!res.ok) {
-        throw new Error('그룹을 불러오는데 실패하였습니다.');
-    }
-
-    return res.json();
+export default async function getGroups(userid: string) {
+    return axios.get(`${process.env.NEXT_PUBLIC_SITE_URL}/api/groups/${userid}`).then((res) => res.data);
 }
