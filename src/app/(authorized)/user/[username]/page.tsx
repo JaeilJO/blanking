8db79e702a52941b470a185e804074c199a4026e
JaeilJo <whwodlf97@gmail.com>
2024-01-17 15:25:02 +0900

    refacotr : group page 정리

--- src/app/(authorized)/user/[username]/page.tsx
+++ src/app/(authorized)/user/[username]/page.tsx
@@ -1,32 +1,10 @@
-import { redirect } from 'next/navigation';
-
 import style from './page.module.scss';
 import NoGroup from '@/components/NoGroup';
 
-import { getServerSession } from 'next-auth';
-import { config } from '@/utils/auth';
-
-export default async function Page({ params }: { params: { username: string } }) {
-    const session = await getServerSession(config);
-    const userid = session?.user?.id as string;
-
-    //Get Groups
-    const res = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api/groups/${userid}`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            cookie: `id=${session?.user?.id}`,
-        },
-        credentials: 'include',
-    });
-
-    const groups = await res.json();
-
-    if (groups.length === 0) {
-        return (
-            <div className={style.userpage_wrapper}>
-                <NoGroup />
-            </div>
-        );
-    }
+export default async function Page() {
+    return (
+        <div className={style.userpage_wrapper}>
+            <NoGroup />
+        </div>
+    );
 }
