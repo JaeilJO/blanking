ee6a68c41e279326e93f7e450082ec2c7725b6fc
JaeilJo <whwodlf97@gmail.com>
2024-01-18 23:08:49 +0900

    feat : Delete 버튼 추가

--- src/components/Modals/DeletePageModal/index.tsx
+++ src/components/Modals/DeletePageModal/index.tsx
@@ -15,6 +15,7 @@ function DeletePageModal() {
     const pagename = decodeURIComponent(searchParams.get('pagename') as string);
     const groupname = decodeURIComponent(searchParams.get('groupname') as string);
 
+    console.log(pagename, groupname);
     const { error, success } = useAlertStore((state) => state);
 
     const queryClient = useQueryClient();
