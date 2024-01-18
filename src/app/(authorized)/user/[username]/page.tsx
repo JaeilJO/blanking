7a87d1e9db298956dd2aa9607cbcc2cbb03ad261
JaeilJo <whwodlf97@gmail.com>
2024-01-19 01:08:26 +0900

    faet : 404 page제작

--- src/app/(authorized)/user/[username]/page.tsx
+++ src/app/(authorized)/user/[username]/page.tsx
@@ -1,7 +1,20 @@
+import { getServerSession } from 'next-auth';
 import style from './page.module.scss';
 import NoGroup from '@/components/NoGroup';
+import { config } from '@/utils/auth';
+import { headers } from 'next/headers';
+import { redirect } from 'next/navigation';
 
 export default async function Page() {
+    const session = await getServerSession(config);
+    const nameInurl = headers().get('next-url')?.split('/')[2];
+
+    const username = session?.user?.name;
+
+    if (nameInurl !== username) {
+        redirect('/not-found');
+    }
+
     return (
         <div className={style.userpage_wrapper}>
             <NoGroup />
