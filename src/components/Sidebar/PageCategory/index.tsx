fe792b8f139273ce866623df3ff47da7c599bef0
JaeilJO <whwodlf97@gmail.com>
2024-01-19 16:28:30 +0900

    fix : No Group Component에서 Make Group글씨가 줄바꿈되는 것 수정

--- src/components/Sidebar/PageCategory/index.tsx
+++ src/components/Sidebar/PageCategory/index.tsx
@@ -1,46 +1,51 @@
-'use clinet';
+"use clinet";
 
-import style from './index.module.scss';
+import style from "./index.module.scss";
 
-import { useParams } from 'next/navigation';
+import { useParams } from "next/navigation";
 
-import PageCategoryItem from './PageCategoryItem';
-import CreatePageButton from './CreatePageButton';
+import PageCategoryItem from "./PageCategoryItem";
+import CreatePageButton from "./CreatePageButton";
 
 interface PageCategoryProps {
-    username: string;
-    groupname: string;
-    pages: any[];
-    iscurrent_group: boolean;
+  username: string;
+  groupname: string;
+  pages: any[];
+  iscurrent_group: boolean;
 }
 
-function PageCategory({ username, groupname, pages, iscurrent_group }: PageCategoryProps) {
-    const param = useParams();
+function PageCategory({
+  username,
+  groupname,
+  pages,
+  iscurrent_group,
+}: PageCategoryProps) {
+  const param = useParams();
 
-    const current_page_name = decodeURIComponent(param.page as string);
-    const current_group_name = decodeURIComponent(param.group as string);
+  const current_page_name = decodeURIComponent(param.page as string);
+  const current_group_name = decodeURIComponent(param.group as string);
 
-    const pageLength = pages ? pages.length : 0;
+  const pageLength = pages ? pages.length : 0;
 
-    return (
-        <ul
-            className={style.page_category_wrapper}
-            style={{ height: `${iscurrent_group ? pageLength * 40 + 40 : 0}px` }}
-        >
-            {pages?.map((page: any) => (
-                <PageCategoryItem
-                    key={page.pagename}
-                    current_page_name={current_page_name}
-                    pagename={page.pagename}
-                    current_group_name={current_group_name}
-                    groupname={groupname}
-                    username={username}
-                />
-            ))}
+  return (
+    <ul
+      className={style.page_category_wrapper}
+      style={{ height: `${iscurrent_group ? pageLength * 40 + 40 : 0}px` }}
+    >
+      {pages?.map((page: any) => (
+        <PageCategoryItem
+          key={page.pagename}
+          current_page_name={current_page_name}
+          pagename={page.pagename}
+          current_group_name={current_group_name}
+          groupname={groupname}
+          username={username}
+        />
+      ))}
 
-            <CreatePageButton current_group_name={current_group_name} />
-        </ul>
-    );
+      <CreatePageButton current_group_name={current_group_name} />
+    </ul>
+  );
 }
 
 export default PageCategory;
