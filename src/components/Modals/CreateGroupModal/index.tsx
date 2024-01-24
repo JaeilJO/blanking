949d4dfb56c98742af8b5c839dadef47d61c20f1
JaeilJo <whwodlf97@gmail.com>
2024-01-25 02:33:16 +0900

    fix : 이름에 특수문자가 있는 경우를 만듦
    - 에러알림 나오도록 설정

--- src/components/Modals/CreateGroupModal/index.tsx
+++ src/components/Modals/CreateGroupModal/index.tsx
@@ -38,22 +38,23 @@ function CreateGroupModal() {
 
     const { mutate } = useMutation({
         mutationFn: () => createGroup({ subkey, groupname }),
-        onMutate: () => {
-            console.log('groupname', groupname);
-        },
+
         onSuccess: () => {
             success('그룹 생성이 완료되었습니다.');
             queryClient.invalidateQueries({ queryKey: ['navigation'] });
             router.back();
         },
         onError: (e: AxiosError) => {
-            console.log(e);
             if (e.request?.status === 409) {
                 error('이미 존재하는 그룹입니다.');
             }
 
             if (e.request?.status === 400) {
                 error('그룹 생성에 실패했습니다. Modal창을 껐다가 다시 시도해주시겠습니까?');
+            }
+
+            if (e.request?.status === 422) {
+                error('특수문자를 제외한 영어 대 소문자 혹은 한글만을 사용해주세요');
             }
         },
     });
--- src/utils/dist/containsSpecialCharacters.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-function containsSpecialCharacters(inputString) {
-    var regex = /[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/;
-    return regex.test(inputString);
-}
-exports["default"] = containsSpecialCharacters;
