5221eaa75957221b161d7c41f5399e4244d71966
JaeilJo <whwodlf97@gmail.com>
2024-01-25 04:11:38 +0900

    feat : useChangeGroupName 커스텀 훅 제작

--- src/components/Modals/ChangeGroupNameModal/index.tsx
+++ src/components/Modals/ChangeGroupNameModal/index.tsx
@@ -2,10 +2,8 @@
 
 //Utils
 import { useCallback, useState } from 'react';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
+
 import { useRouter, useSearchParams } from 'next/navigation';
-import { useAlertStore } from '@/zustand/alertStore';
-import { changeGroupName } from '@/services/changeGroupName';
 
 //Style
 import style from './index.module.scss';
@@ -13,14 +11,12 @@ import style from './index.module.scss';
 //Components
 import ModalBackground from '../ModalBackground';
 import { useSession } from 'next-auth/react';
-import { AxiosError } from 'axios';
+import useChangeGroupName from '@/hooks/useChangeGroupName';
 
 function ChangeGroupNameModal() {
     const searchParams = useSearchParams();
     const session = useSession();
     const router = useRouter();
-    const { error, success } = useAlertStore((state) => state);
-    const queryClient = useQueryClient();
 
     //Subkey
     const subkey = session.data?.user.subkey as string;
@@ -38,34 +34,7 @@ function ChangeGroupNameModal() {
         [newGroupName]
     );
 
-    const { mutate } = useMutation({
-        mutationFn: () => changeGroupName({ groupname, new_groupname: newGroupName, subkey }),
-
-        onSuccess: (res) => {
-            const message = res.data;
-            success(message);
-            queryClient.invalidateQueries({ queryKey: ['navigation'] });
-            router.back();
-        },
-
-        onError: (e: AxiosError) => {
-            if (e.request?.status === 409) {
-                error('Group이름 변경에 실패했습니다. 다시 시도해주시겠습니까?');
-            }
-
-            if (e.request?.status === 403) {
-                error('이미 사용되고 있는 Group 이름입니다.');
-            }
-
-            if (e.request?.status === 422) {
-                error('특수문자를 제외한 영어 대 소문자 혹은 한글만을 사용해주세요');
-            }
-
-            if (e.request?.status === 423) {
-                error('기존 Group 이름과 동일합니다.');
-            }
-        },
-    });
+    const { mutate } = useChangeGroupName({ groupname, new_groupname: newGroupName, subkey, router });
 
     const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
