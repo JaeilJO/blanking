b0264f87bc307f728672821e98ec153d476a3b77
JaeilJo <whwodlf97@gmail.com>
2024-01-25 04:51:39 +0900

    refactor : sidebar middle사이증일때 자동으로 close되는 기능 추가

--- src/components/Sidebar/SidebarLayout/sidebarLayout.tsx
+++ src/components/Sidebar/SidebarLayout/sidebarLayout.tsx
@@ -6,11 +6,26 @@ import style from './index.module.scss';
 import classNames from 'classnames/bind';
 
 import SideBarStatusButton from '../SideBarStatusButton';
+import { useEffect } from 'react';
 
 const cn = classNames.bind(style);
 
 function SideBarLayout({ subkey }: { subkey: string }) {
     const { status, close, open } = useSideBarStatusStore((state) => state);
+    const handleResize = () => {
+        if (window.innerWidth < 768) {
+            close();
+        } else {
+            open();
+        }
+    };
+    useEffect(() => {
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
 
     return (
         <nav className={cn({ sidebar_wrapper: true, close: status })}>
