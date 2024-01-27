282bb2dc35faa371aa639b72eb8610b3f0919edf
JaeilJo <whwodlf97@gmail.com>
2024-01-27 16:27:01 +0900

    feat : Logo Animation 추가

--- src/components/AuthSlide/SlideItems/Logo/index.tsx
+++ src/components/AuthSlide/SlideItems/Logo/index.tsx
@@ -1,10 +1,39 @@
+'use client';
+
 import SlideItemWrapper from '../../SlideItemWrapper';
+
+import { Player, Controls } from '@lottiefiles/react-lottie-player';
+import logoAnimation from '@/public/logo.json';
+import { useEffect, useRef, useState } from 'react';
+
 import style from './index.module.scss';
 
 function Logo() {
+    const animatioRef = useRef<Player>(null);
+    const [stop, setStop] = useState(false);
+
+    const onEvent = () => {
+        if (animatioRef.current?.state.seeker === 20) {
+            setStop(true);
+        }
+    };
+
+    useEffect(() => {
+        if (stop) {
+            animatioRef.current?.pause();
+        }
+    }, [stop]);
     return (
         <SlideItemWrapper>
-            <div className={style.logotext}>Blanking</div>
+            <div className={style['player-wrapper']}>
+                <Player
+                    autoplay
+                    ref={animatioRef}
+                    src={logoAnimation}
+                    onEvent={onEvent}
+                    style={{ width: '100%', maxWidth: '800px' }}
+                />
+            </div>
         </SlideItemWrapper>
     );
 }
