84ead429a714b2cfa83c531801376fd61117354e
JaeilJo <whwodlf97@gmail.com>
2024-01-30 15:25:31 +0900

    fix : getGroups 수정

--- src/services/getGroups.ts
+++ src/services/getGroups.ts
@@ -1,5 +1,13 @@
 import axios from 'axios';
 
 export default async function getGroups(subkey: string) {
-    return axios.get(`${process.env.NEXT_PUBLIC_SITE_URL}/api/groups/${subkey}`).then((res) => res.data);
+    return axios
+        .get(`${process.env.NEXT_PUBLIC_SITE_URL}/api/groups/${subkey}`, {
+            headers: {
+                'Cache-Control': 'no-cache',
+                Pragma: 'no-cache',
+                Expires: '0',
+            },
+        })
+        .then((res) => res.data);
 }
