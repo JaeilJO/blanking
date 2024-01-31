3896ae54afbaa17ec191a87840356a5e7d7068c5
JaeilJo <whwodlf97@gmail.com>
2024-01-31 18:12:18 +0900

    feat : Scroll Guide Box	생성 - AuthComponent가 최대 height보다 줄어들면 스크롤을 할 수 있다는 문구를 보여줌

--- src/components/AuthComponent/SignUpComponent/index.tsx
+++ src/components/AuthComponent/SignUpComponent/index.tsx
@@ -1,11 +1,40 @@
+'use client';
+
+import { useEffect, useRef, useState } from 'react';
 import { AuthComponent } from '../AuthCommon';
 import style from './index.module.scss';
+import ScrollGuideBox from '../ScrollGuideBox';
 
 const socials = ['google'];
 
 function SignUpComponent() {
+    const [isHeightBelowMax, setIsHeightBelowMax] = useState(false);
+    const ref = useRef<HTMLDivElement>(null);
+
+    const scrollHandler = () => {
+        setIsHeightBelowMax(false);
+    };
+
+    useEffect(() => {
+        const maxHeight = 820;
+
+        const currentHeight = ref.current?.offsetHeight;
+
+        if (currentHeight !== undefined && maxHeight > currentHeight) {
+            setIsHeightBelowMax(true);
+        }
+    }, []);
+
+    useEffect(() => {
+        if (!isHeightBelowMax) {
+            ref.current?.addEventListener('scroll', scrollHandler);
+        }
+
+        return () => ref.current?.removeEventListener('scroll', scrollHandler);
+    }, []);
+
     return (
-        <div className={style.auth_wrapper}>
+        <div ref={ref} className={style.auth_wrapper}>
             <AuthComponent.Title title="Sign Up" welcome_message="환영합니다." />
 
             <AuthComponent.SignUpForm />
@@ -13,6 +42,8 @@ function SignUpComponent() {
             <AuthComponent.Social socials={socials} />
 
             <AuthComponent.AccountInquery authType={'signup'} />
+
+            {isHeightBelowMax && <ScrollGuideBox />}
         </div>
     );
 }
