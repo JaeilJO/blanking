05e85caeea26b0a29ccc2c9617d1c68ed873ed45
JaeilJo <whwodlf97@gmail.com>
2024-02-01 23:48:22 +0900

    refactor : Auth 변경사항 SignInComponent와 SignupComponent에 적용

--- src/components/AuthComponent/SignInComponent/index.tsx
+++ src/components/AuthComponent/SignInComponent/index.tsx
@@ -1,11 +1,17 @@
 'use client';
 
+// Utils
 import Link from 'next/link';
-import style from './index.module.scss';
-import { AuthComponent } from '../AuthCommon';
 import social_login_list from '@/utils/socialLoginList';
 import { useEffect, useRef, useState } from 'react';
+
+// Style
+import style from './index.module.scss';
+
+// Components
+import AuthCommon from '../AuthCommon';
 import ScrollGuideBox from '../ScrollGuideBox';
+import SignInForm from '../SignInForm';
 
 function SignInComponent() {
     const [isHeightBelowMax, setIsHeightBelowMax] = useState(false);
@@ -35,17 +41,17 @@ function SignInComponent() {
 
     return (
         <div ref={ref} className={style.auth_wrapper}>
-            <AuthComponent.Title title="Sign In" welcome_message="다시 찾아와 주셔서 감사합니다." />
+            <AuthCommon.Title title="Sign In" welcome_message="다시 찾아와 주셔서 감사합니다." />
 
-            <AuthComponent.SignInForm />
+            <SignInForm />
 
-            <Link className={style.link_text} href={'#'} title="아직 구현이 안됐습니다">
+            <Link className={style.link_text} href={'/auth/changePassword'}>
                 비밀번호를 잊어버리셨나요?
             </Link>
 
-            <AuthComponent.Social socials={social_login_list} />
+            <AuthCommon.Social socials={social_login_list} />
 
-            <AuthComponent.AccountInquery authType={'signin'} />
+            <AuthCommon.AccountInquery authType={'signin'} />
 
             {isHeightBelowMax && <ScrollGuideBox />}
         </div>
