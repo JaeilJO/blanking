2861f5bdd54fba86e5b5a76ae21feb10a46ce8e6
JaeilJO <whwodlf97@gmail.com>
2024-02-03 16:22:46 +0900

    fix : Mobile 문제 해결로 Mobile 접근을 막는 로직 제거

--- src/components/WelcomePage/index.tsx
+++ src/components/WelcomePage/index.tsx
@@ -1,39 +1,27 @@
-'use client';
+"use client";
 
-import Image from 'next/image';
-import Link from 'next/link';
-import Logo from '@/public/Logo.png';
-import style from './index.module.scss';
-import MobileModal from '../MobileModal';
-import { useEffect, useState } from 'react';
+import Image from "next/image";
+import Link from "next/link";
+import Logo from "@/public/Logo.png";
+import style from "./index.module.scss";
 
 function WelcomePage() {
-    const [isMobile, setIsMobile] = useState(false);
+  return (
+    <>
+      <div className={style["welcome-wrapper"]}>
+        <Image className={style.logo} src={Logo} width={300} alt="Logo" />
 
-    useEffect(() => {
-        let userAgent = navigator.userAgent;
-        if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent)) {
-            setIsMobile(true);
-        }
-    }, []);
-
-    return (
-        <>
-            {isMobile && <MobileModal />}
-            <div className={style['welcome-wrapper']}>
-                <Image className={style.logo} src={Logo} width={300} alt="Logo" />
-
-                <div className={style['link-wrapper']}>
-                    <Link className={style.link} href={'/auth/signin'}>
-                        Sign In
-                    </Link>
-                    <Link className={style.link} href={'/auth/signup'}>
-                        Sign Up
-                    </Link>
-                </div>
-            </div>
-        </>
-    );
+        <div className={style["link-wrapper"]}>
+          <Link className={style.link} href={"/auth/signin"}>
+            Sign In
+          </Link>
+          <Link className={style.link} href={"/auth/signup"}>
+            Sign Up
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default WelcomePage;
