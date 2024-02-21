53d52815b4740a7ca66b278017d623e7c5f20ddb
JaeilJo <whwodlf97@gmail.com>
2024-02-21 16:22:37 +0900

    feat : AuthButton 생성

---
+++ src/components/AuthComponent/Atoms/AuthButton/index.tsx
@@ -0,0 +1,24 @@
+import { ReactNode } from "react";
+import style from "./index.module.scss";
+
+interface AuthButtonProps {
+  value: string;
+
+  /**
+   * React Icon으로 적용해주세요
+   */
+  icon?: ReactNode;
+  type: "submit" | "button";
+  onClick?: () => void;
+}
+
+function AuthButton({ value, icon, type, onClick }: AuthButtonProps) {
+  return (
+    <button className={style["button"]} type={type} onClick={onClick}>
+      <div className={style["icon"]}>{icon}</div>
+      <div>{value}</div>
+    </button>
+  );
+}
+
+export default AuthButton;
