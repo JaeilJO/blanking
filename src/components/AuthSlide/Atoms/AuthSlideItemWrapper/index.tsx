65c9c48e3c69fd3508e3c859a9f286b35b79bf09
JaeilJo <whwodlf97@gmail.com>
2024-02-24 21:19:12 +0900

    feat : Auth Slide Item Wrapper 생성

---
+++ src/components/AuthSlide/Atoms/AuthSlideItemWrapper/index.tsx
@@ -0,0 +1,12 @@
+// Style
+import style from "./index.module.scss";
+
+interface AuthSlideItemWrapperProps {
+  children: React.ReactNode;
+}
+
+function AuthSlideItemWrapper({ children }: AuthSlideItemWrapperProps) {
+  return <div className={style["auth-slide-item-wrapper"]}>{children}</div>;
+}
+
+export default AuthSlideItemWrapper;
