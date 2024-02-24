ed2c58f957f5d56bdf92ee2869fcf3999af81c38
JaeilJo <whwodlf97@gmail.com>
2024-02-24 21:24:59 +0900

    feat : Auth Slide Wrapper 추가

---
+++ src/components/AuthSlide/Atoms/AuthSlideWrapper/index.tsx
@@ -0,0 +1,12 @@
+// Style
+import style from "./index.module.scss";
+
+interface AuthSlideWrapperProps {
+  children: React.ReactNode;
+}
+
+function AuthSlideWrapper({ children }: AuthSlideWrapperProps) {
+  return <div className={style["auth-slide-wrapper"]}>{children}</div>;
+}
+
+export default AuthSlideWrapper;
