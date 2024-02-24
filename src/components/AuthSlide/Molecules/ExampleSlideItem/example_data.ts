9d349b39418f5f2770698dc764b21356e4b49b0e
JaeilJo <whwodlf97@gmail.com>
2024-02-24 21:26:38 +0900

    feat : Example Slide Item 추가

---
+++ src/components/AuthSlide/Molecules/ExampleSlideItem/example_data.ts
@@ -0,0 +1,58 @@
+const exampleData = {
+    time: 12345,
+    blocks: [
+        {
+            id: 'oUq2g_tl8y',
+            type: 'header',
+            data: {
+                text: 'Example',
+                level: 1,
+            },
+        },
+        {
+            id: '"b_Ef74EXfD"',
+            type: 'paragraph',
+            data: {
+                text: 'Blanking을 한번 경험해 보세요',
+            },
+        },
+
+        {
+            id: '2Nc67xN75R',
+            type: 'paragraph',
+            data: {
+                text: `이렇게 <label contenteditable="false" class="blank_style">빈칸</label><input type="checkbox" id="589e8289-32b8-4137-ad46-ce7210dea4bb" class="blank_button">을 만들어볼 수 있답니다`,
+            },
+        },
+        {
+            id: '-kPS9Ug229',
+            type: 'header',
+            data: {
+                text: 'Blank 사용방법',
+                level: 3,
+            },
+        },
+        {
+            id: 'zIcxq-pBvz',
+            type: 'paragraph',
+            data: {
+                text: '1. 글씨를 드레그 해주세요',
+            },
+        },
+        {
+            id: '7dzV3GwBlH',
+            type: 'paragraph',
+            data: {
+                text: '2. Blank Icon을 선택해주세요',
+            },
+        },
+        {
+            id: 'VVLiRikQnj',
+            type: 'paragraph',
+            data: {
+                text: '3. Blank 처리된 글씨를 확인하려면 <b>"클릭"</b>을 하시면 됩니다.',
+            },
+        },
+    ],
+};
+export default exampleData;
