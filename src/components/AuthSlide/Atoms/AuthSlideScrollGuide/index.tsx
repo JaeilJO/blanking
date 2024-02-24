cdd84d5b9fe0f3c86b89eb83c046595ecad76ca7
JaeilJo <whwodlf97@gmail.com>
2024-02-24 21:21:15 +0900

    feat : Auth Slide Scroll Guide 제작

---
+++ src/components/AuthSlide/Atoms/AuthSlideScrollGuide/index.tsx
@@ -0,0 +1,21 @@
+// Style
+import style from "./index.module.scss";
+
+// Icons
+import { SlMouse } from "react-icons/sl";
+
+function AuthSlideScrollGuide() {
+  return (
+    <div className={style["wrapper"]}>
+      <div className={style["icon"]}>
+        <SlMouse />
+      </div>
+
+      <div className={style["title"]}>
+        다음 섹션에서 Blanking을 체험해보세요
+      </div>
+    </div>
+  );
+}
+
+export default AuthSlideScrollGuide;
