18ec6f89e3e501580e58f05935e7f1fe3947d0e9
JaeilJo <whwodlf97@gmail.com>
2024-02-24 21:28:09 +0900

    feat : Auth Slide Teamplate 생성

---
+++ src/components/AuthSlide/Templates/AuthSlideTemplate/index.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+// Components
+import AuthSlideWrapper from "../../Atoms/AuthSlideWrapper";
+import AuthSlideContainer from "../../Molecules/AuthSlideContainer";
+
+function AuthSlideTemplate() {
+  return (
+    <AuthSlideWrapper>
+      <AuthSlideContainer />
+    </AuthSlideWrapper>
+  );
+}
+
+export default AuthSlideTemplate;
