3a2def4e2dd0367df94f2b794414e5b1c37a9289
JaeilJo <whwodlf97@gmail.com>
2024-02-26 22:19:50 +0900

    feat : User Name Page Template 제작 완료

---
+++ src/components/UserNamePage/Templates/UserNamePageTemplate/index.tsx
@@ -0,0 +1,13 @@
+// Components
+import UserNamePageWrapper from "../../Atoms/UserNamePageWrapper";
+import GroupMaker from "../../Organisms/GroupMaker";
+
+function UserNamePageTemplate() {
+  return (
+    <UserNamePageWrapper>
+      <GroupMaker />
+    </UserNamePageWrapper>
+  );
+}
+
+export default UserNamePageTemplate;
