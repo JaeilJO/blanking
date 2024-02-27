c3278864f0c6ea26d220884b6850fdba4426cf6a
JaeilJo <whwodlf97@gmail.com>
2024-02-27 13:24:08 +0900

    feat : Group Page Table Item Wrapper

---
+++ src/components/GroupPage/Atoms/GroupPageTableItemWrapper/index.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import style from "./index.module.scss";
+
+interface GroupPageTableItemWrapperProps {
+  children: React.ReactNode;
+  groupname: string;
+  pagename: string;
+}
+
+function GroupPageTableItemWrapper({
+  children,
+  groupname,
+  pagename,
+}: GroupPageTableItemWrapperProps) {
+  return (
+    <Link href={`${groupname}/${pagename}`} className={style["wrapper"]}>
+      {children}
+    </Link>
+  );
+}
+
+export default GroupPageTableItemWrapper;
