a4d9960183099048580ce997b7d69847c15f6ac3
JaeilJo <whwodlf97@gmail.com>
2024-02-28 17:49:44 +0900

    refactor : Input 공통 컴포넌트 password인 경우 custom 버튼 생성

--- src/components/Atoms/Input/index.stories.tsx
+++ src/components/Atoms/Input/index.stories.tsx
@@ -15,6 +15,58 @@ export const Primary: Story = {
     type: "text",
     name: "name",
     required: true,
+    theme: "primary",
+    onChange: () => {},
+  },
+};
+
+export const Default: Story = {
+  args: {
+    type: "text",
+    name: "name",
+    required: true,
+    theme: "default",
+    onChange: () => {},
+  },
+};
+
+export const Gray: Story = {
+  args: {
+    type: "text",
+    name: "name",
+    required: true,
+    theme: "gray",
+    onChange: () => {},
+  },
+};
+
+export const Warn: Story = {
+  args: {
+    type: "text",
+    name: "name",
+    required: true,
+    theme: "warn",
+    onChange: () => {},
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    type: "text",
+    name: "name",
+    disabled: true,
+    required: true,
+    theme: "default",
+    onChange: () => {},
+  },
+};
+
+export const Password: Story = {
+  args: {
+    type: "password",
+    name: "password",
+    required: true,
+    theme: "default",
     onChange: () => {},
   },
 };
--- src/components/Atoms/Input/index.tsx
+++ src/components/Atoms/Input/index.tsx
@@ -2,12 +2,13 @@
 
 import style from "./index.module.scss";
 import classNames from "classnames/bind";
-import { ChangeEventHandler } from "react";
+import { ChangeEventHandler, useState } from "react";
+import { BsEye, BsEyeSlash } from "react-icons/bs";
 
 interface InputProps {
-  type: "text" | "password" | "email";
   name: string;
 
+  type?: "text" | "password" | "email";
   placeholder?: string;
   required?: boolean;
   textAlign?: "left" | "center" | "right";
@@ -20,35 +21,59 @@ interface InputProps {
 const cn = classNames.bind(style);
 
 function Input({
-  type,
-  placeholder,
-  required,
-  onChange,
   name,
-  disabled,
+
+  type = "text",
+  placeholder,
+  required = true,
   textAlign = "left",
+  disabled = false,
   theme = "default",
+
+  onChange,
 }: InputProps) {
-  const className = cn("input", {
+  const inputClassName = cn("input", {
     left: textAlign === "left",
     center: textAlign === "center",
     right: textAlign === "right",
+  });
 
+  const inputWrapperClassName = cn("input-wrapper", {
     default: theme === "default",
     primary: theme === "primary",
     gray: theme === "gray",
     warn: theme === "warn",
+
+    disabled: disabled,
   });
+  const [isHide, setIsHide] = useState(false);
+
+  const passwordToggleHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setIsHide(!isHide);
+  };
+
   return (
-    <input
-      className={className}
-      type={type}
-      name={name}
-      required={required}
-      placeholder={placeholder}
-      onChange={onChange}
-      disabled={disabled}
-    />
+    <div className={inputWrapperClassName}>
+      <input
+        className={inputClassName}
+        type={type === "password" ? (isHide ? "text" : "password") : type}
+        name={name}
+        required={required}
+        placeholder={placeholder}
+        onChange={onChange}
+        disabled={disabled}
+      />
+
+      {type === "password" && (
+        <button
+          className={style["password-button"]}
+          onClick={passwordToggleHandler}
+        >
+          {isHide ? <BsEyeSlash /> : <BsEye />}
+        </button>
+      )}
+    </div>
   );
 }
 
