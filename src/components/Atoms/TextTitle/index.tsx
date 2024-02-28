18f8d2cc82a8b2d9f6586fc763436b250a1138d7
JaeilJo <whwodlf97@gmail.com>
2024-02-28 21:43:49 +0900

    refactor : text Title Class Name 정리

--- src/components/Atoms/TextTitle/index.tsx
+++ src/components/Atoms/TextTitle/index.tsx
@@ -16,24 +16,15 @@ function TextTitle({
 }: TextTitleProps) {
   // ClassName 설정
 
-  const textTitleClassName = cn("title", `title-${level}`, {
-    light: weight === "light",
-    regular: weight === "regular",
-    bold: weight === "bold",
-
-    left: textAlign === "left",
-    center: textAlign === "center",
-    right: textAlign === "right",
-
-    displayInline: display === "inline",
-
-    "defulat-color": color === "default",
-    "primary-color": color === "primary",
-    "light-gray-color": color === "light-gray",
-    "gray-color": color === "gray",
-
-    "no-drag": noDrag,
-  });
+  const textTitleClassName = cn(
+    "title",
+    `title-${level}`,
+    textAlign,
+    display === "inline" && "displayInline",
+    weight,
+    `${color}-color`,
+    { "no-drag": noDrag }
+  );
 
   return <h1 className={textTitleClassName}>{children}</h1>;
 }
