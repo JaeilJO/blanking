64614fb78433f023b5264bb913974b83e5f221f8
JaeilJo <whwodlf97@gmail.com>
2024-02-29 15:40:11 +0900

    refactor : common type 수정
    - textAlign도 공통타입에 추가

--- src/components/Atoms/Text/Common/common.type.ts
+++ src/components/Atoms/Text/Common/common.type.ts
@@ -1,6 +1,8 @@
 type CommonTextTypes = {
   children: React.ReactNode;
   block?: boolean;
+  weight?: "light" | "regular" | "bold";
+  textAlign?: "left" | "center" | "right";
   theme?: "default" | "primary" | "gray " | "warn" | "light-gray";
   noDrag?: boolean;
 };
--- src/components/Atoms/Text/TextBody/index.type.ts
+++ src/components/Atoms/Text/TextBody/index.type.ts
@@ -1,7 +1,5 @@
 import CommonTextTypes from "../Common/common.type";
 
 export interface TextSubTitleProps extends CommonTextTypes {
-  weight?: "light" | "regular" | "bold";
-  textAlign?: "left" | "center" | "right";
   level: "01" | "02";
 }
