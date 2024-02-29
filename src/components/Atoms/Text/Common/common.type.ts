55dd615c7fbbbf498827bd64c5f0ea0c2e627167
JaeilJo <whwodlf97@gmail.com>
2024-02-29 15:02:05 +0900

    feat : Text 공통 Component 공통 타입과 공통 스타일 생성
    - 타입은 extends로 확장해서 사용가능
    - 스타일은 include로 불러와서 사용가능

---
+++ src/components/Atoms/Text/Common/common.type.ts
@@ -0,0 +1,8 @@
+type CommonTextTypes = {
+  children: React.ReactNode;
+  block?: boolean;
+  theme?: "default" | "primary" | "gray " | "warn" | "light-gray";
+  noDrag?: boolean;
+};
+
+export default CommonTextTypes;
