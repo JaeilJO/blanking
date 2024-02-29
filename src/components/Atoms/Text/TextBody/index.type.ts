29081434e4f78681c368994314cafdb84095f28a
JaeilJo <whwodlf97@gmail.com>
2024-02-29 15:31:19 +0900

    fix : TextBody type weight에 bold추가

--- src/components/Atoms/Text/TextBody/index.type.ts
+++ src/components/Atoms/Text/TextBody/index.type.ts
@@ -1,7 +1,7 @@
 import CommonTextTypes from "../Common/common.type";
 
 export interface TextSubTitleProps extends CommonTextTypes {
-  weight?: "light" | "regular" | "blod";
+  weight?: "light" | "regular" | "bold";
   textAlign?: "left" | "center" | "right";
   level: "01" | "02";
 }
