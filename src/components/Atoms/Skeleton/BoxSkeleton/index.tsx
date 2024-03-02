cce538d6f4e345527aa62add97a2e08140e19993
JaeilJo <whwodlf97@gmail.com>
2024-03-02 23:38:38 +0900

    feat : Commit이 안된 Box Skeleton index file 커밋

--- src/components/Atoms/Skeleton/BoxSkeleton/index.tsx
+++ src/components/Atoms/Skeleton/BoxSkeleton/index.tsx
@@ -1,6 +1,10 @@
 import style from "./index.module.scss";
 
 export interface BoxSkeletonProps {
+  /**
+   * 기존의 widht값과 똑같이 적용됩니다
+   * 100%, 100dvh 등 적용가능
+   */
   width: string;
   height: string;
 }
