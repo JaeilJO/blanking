16e00ff7c6aac60745560a016ce902a2bb408ac9
JaeilJo <whwodlf97@gmail.com>
2024-03-06 22:29:31 +0900

    feat : Avatar 생성

---
+++ src/components/Atoms/Avatar/index.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import Avatar from ".";
+
+const meta: Meta<typeof Avatar> = {
+  component: Avatar,
+  tags: ["autodocs"],
+};
+
+export default meta;
+type Story = StoryObj<typeof Avatar>;
+
+export const Custom: Story = {
+  args: {
+    name: "Custom",
+  },
+};
+
+export const Primary: Story = {
+  render: () => (
+    <div>
+      <Avatar name="Samll" size={"small"} />
+      <Avatar name="Medium" size={"medium"} />
+      <Avatar name="Large" size={"large"} />
+    </div>
+  ),
+};
--- src/components/Atoms/Avatar/index.tsx
+++ src/components/Atoms/Avatar/index.tsx
@@ -1,23 +1,58 @@
+"use client";
+
 import classNames from "classnames/bind";
 import style from "./index.module.scss";
-
-interface AvatarProps {
-  name: string;
-  fontSize:
-    | "title-01"
-    | "title-02"
-    | "title-03"
-    | "subTitle-01"
-    | "subTitle-02"
-    | "body-01"
-    | "body-02";
-}
+import AvatarProps from "./index.type";
 
 const cn = classNames.bind(style);
 
-function Avatar({ name, fontSize = "subTitle-01" }: AvatarProps) {
-  const avatarClass = cn("avatar", fontSize);
-  return <div className={avatarClass}>{name[0]}</div>;
+function Avatar({
+  name,
+  size,
+  fontSize,
+  marginBottom,
+  marginLeft,
+  marginRight,
+  marginTop,
+  disabled,
+  ...props
+}: AvatarProps) {
+  // size가 number이면 fontSize가 필수, fontSize가 있으면 size가 필수
+  if (typeof size === "number" && !fontSize) {
+    throw new Error("size가 number이면 fontSize가 필수입니다.");
+  }
+
+  if (fontSize && typeof size !== "number") {
+    throw new Error("fontSize가 있으면 size가 number여야 합니다.");
+  }
+
+  const remSize = typeof size === "number" ? size / 10 : null;
+
+  const avatarClass = cn(
+    "avatar",
+    fontSize,
+    marginBottom,
+    marginLeft,
+    marginRight,
+    marginTop,
+
+    {
+      small: size === "small",
+      medium: size === "medium",
+      large: size === "large",
+    }
+  );
+
+  return (
+    <button
+      className={avatarClass}
+      disabled={disabled}
+      style={{ width: `${remSize}rem`, height: `${remSize}rem` }}
+      {...props}
+    >
+      {name[0]}
+    </button>
+  );
 }
 
 export default Avatar;
