f871811b242307a72d4cc57d777f7c23d7b83415
JaeilJO <whwodlf97@gmail.com>
2024-03-06 17:59:22 +0900

    feat : Margin type 생성

--- src/style/style.type.ts
+++ src/style/style.type.ts
@@ -8,3 +8,43 @@ export type FontSize =
   | "body-02";
 
 export type FontWeight = "font-light" | "font-regular" | "font-bold";
+
+export type MarginTop =
+  | "mt-0"
+  | "mt-1"
+  | "mt-2"
+  | "mt-3"
+  | "mt-4"
+  | "mt-5"
+  | "mt-6"
+  | "mt-7";
+
+export type MarginBottom =
+  | "mb-0"
+  | "mb-1"
+  | "mb-2"
+  | "mb-3"
+  | "mb-4"
+  | "mb-5"
+  | "mb-6"
+  | "mb-7";
+
+export type MarginLeft =
+  | "ml-0"
+  | "ml-1"
+  | "ml-2"
+  | "ml-3"
+  | "ml-4"
+  | "ml-5"
+  | "ml-6"
+  | "ml-7";
+
+export type MarginRight =
+  | "mr-0"
+  | "mr-1"
+  | "mr-2"
+  | "mr-3"
+  | "mr-4"
+  | "mr-5"
+  | "mr-6"
+  | "mr-7";
