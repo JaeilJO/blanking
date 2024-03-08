b7d76e04aa650d58e4249db19b62428f010d28af
JaeilJO <whwodlf97@gmail.com>
2024-03-09 01:14:37 +0900

    refacotor : Line Button 리팩토링

--- src/components/Atoms/Button/LineButton/index.stories.tsx
+++ src/components/Atoms/Button/LineButton/index.stories.tsx
@@ -1,9 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
 import LineButton from ".";
-import { BsGoogle } from "react-icons/bs";
 
-//👇 This default export determines where your story goes in the story list
 const meta: Meta<typeof LineButton> = {
   component: LineButton,
   tags: ["autodocs"],
@@ -20,185 +18,35 @@ export const Custom: Story = {
 
 export const Theme: Story = {
   render: () => (
-    <div style={{ display: "flex", gap: "24px" }}>
-      <LineButton theme="primary">Primary</LineButton>
-      <LineButton theme="gray">Gray</LineButton>
-      <LineButton theme="black">Black</LineButton>
-      <LineButton theme="light-gray">Light Gray</LineButton>
-      <LineButton theme="red">Red</LineButton>
-      <LineButton theme="yellow">Yellow</LineButton>
-      <LineButton theme="green">Green</LineButton>
-      <LineButton theme="white">White</LineButton>
-    </div>
-  ),
-};
-
-export const Display: Story = {
-  render: () => (
-    <>
-      <LineButton display="block" marginBottom="mb-3" theme="primary">
+    <div>
+      <LineButton
+        theme="primary"
+        marginBottom="mb-3"
+        borderWidth="border-thick"
+      >
         Primary
       </LineButton>
-
-      <LineButton display="inline" theme="gray">
+      <LineButton theme="gray" marginBottom="mb-3">
         Gray
       </LineButton>
-    </>
-  ),
-};
-
-export const Icon: Story = {
-  args: {
-    icon: <BsGoogle />,
-    children: "Icon Text",
-  },
-};
-
-export const BorderWidth: Story = {
-  render: () => (
-    <div style={{ display: "flex", flexWrap: "wrap", gap: "24px" }}>
-      <LineButton theme="primary" borderWidth="border-thin">
-        Thin Border
+      <LineButton theme="black" marginBottom="mb-3">
+        Black
       </LineButton>
-      <LineButton theme="primary" borderWidth="border-medium">
-        Medium Border
+      <LineButton theme="light-gray" marginBottom="mb-3">
+        Light Gray
       </LineButton>
-      <LineButton theme="primary" borderWidth="border-thick">
-        Thick Border
+      <LineButton theme="red" marginBottom="mb-3">
+        Red
+      </LineButton>
+      <LineButton theme="yellow" marginBottom="mb-3">
+        Yellow
+      </LineButton>
+      <LineButton theme="green" marginBottom="mb-3">
+        Green
+      </LineButton>
+      <LineButton theme="white" marginBottom="mb-3">
+        White
       </LineButton>
     </div>
   ),
-};
-
-export const FontSize: Story = {
-  render: () => {
-    return (
-      <div>
-        <LineButton
-          display="inline"
-          marginLeft="ml-3"
-          fontSize="tit-01"
-          theme="primary"
-        >
-          tit-01
-        </LineButton>
-        <LineButton
-          display="inline"
-          marginLeft="ml-3"
-          fontSize="tit-02"
-          theme="primary"
-        >
-          tit-02
-        </LineButton>
-        <LineButton
-          display="inline"
-          marginLeft="ml-3"
-          fontSize="tit-03"
-          theme="primary"
-        >
-          tit-03
-        </LineButton>
-        <LineButton
-          display="inline"
-          marginLeft="ml-3"
-          fontSize="sub-01"
-          theme="primary"
-        >
-          sub-01
-        </LineButton>
-        <LineButton
-          display="inline"
-          marginLeft="ml-3"
-          fontSize="sub-02"
-          theme="primary"
-        >
-          sub-02
-        </LineButton>
-        <LineButton
-          display="inline"
-          marginLeft="ml-3"
-          fontSize="body-01"
-          theme="primary"
-        >
-          body-01
-        </LineButton>
-        <LineButton
-          display="inline"
-          marginLeft="ml-3"
-          fontSize="body-02"
-          theme="primary"
-        >
-          body-02
-        </LineButton>
-      </div>
-    );
-  },
-};
-
-export const FontWeight: Story = {
-  render: () => (
-    <div>
-      <LineButton
-        marginLeft="ml-3"
-        display="inline"
-        fontWeight="font-bold"
-        theme="primary"
-      >
-        Bold
-      </LineButton>
-      <LineButton
-        marginLeft="ml-3"
-        display="inline"
-        fontWeight="font-light"
-        theme="primary"
-      >
-        Light
-      </LineButton>
-      <LineButton
-        marginLeft="ml-3"
-        display="inline"
-        fontWeight="font-regular"
-        theme="primary"
-      >
-        Regular
-      </LineButton>
-    </div>
-  ),
-};
-export const TextAlign: Story = {
-  render: () => (
-    <div>
-      <LineButton
-        marginBottom="mb-3"
-        display="block"
-        textAlign="text-left"
-        theme="primary"
-      >
-        Left Align
-      </LineButton>
-      <LineButton
-        marginBottom="mb-3"
-        display="block"
-        textAlign="text-center"
-        theme="primary"
-      >
-        Center Align
-      </LineButton>
-      <LineButton
-        marginBottom="mb-3"
-        display="block"
-        textAlign="text-right"
-        theme="primary"
-      >
-        Right Align
-      </LineButton>
-    </div>
-  ),
-};
-
-export const Disabled: Story = {
-  args: {
-    children: "Disabled",
-    disabled: true,
-  },
 };
--- src/components/Atoms/Button/LineButton/index.tsx
+++ src/components/Atoms/Button/LineButton/index.tsx
@@ -1,53 +1,66 @@
-import classNames from "classnames/bind";
+import { BaseButtonProps } from "../BaseButton/index.type";
+import BaseButton, { buttonColorTheme } from "../BaseButton";
 
-import style from "./index.module.scss";
-import LineButtonProps from "./index.type";
+// 사용자가 지정하면 안되는 부분 지정
+interface LineButtonProps
+  extends Omit<
+    BaseButtonProps,
+    | "backgroundColor"
+    | "borderColor"
+    | "fontColor"
+    | "paddingBottom"
+    | "paddingLeft"
+    | "paddingRight"
+    | "paddingTop"
+  > {
+  //현재 버튼이 가지고 있는 고유 타입
+  theme: buttonColorTheme;
+}
 
-const cn = classNames.bind(style);
+function LineButton(props: LineButtonProps) {
+  const {
+    theme = "primary",
+    display = "block",
 
-function LineButton({
-  theme,
-  display,
-  fontSize,
-  fontWeight = "font-light",
-  textAlign,
-  icon,
-  children,
-  borderWidth,
-  marginTop,
-  marginBottom,
-  marginLeft,
-  marginRight,
-  disabled,
-  type = "button",
-  ...props
-}: LineButtonProps) {
+    fontSize = "body-01",
+
+    borderWidth = "border-thin",
+
+    marginBottom,
+    marginLeft,
+    marginRight,
+    marginTop,
+
+    icon,
+
+    children,
+
+    ...otherProps
+  } = props;
+
   return (
-    <button
-      type={type}
-      className={cn(
-        "button",
-        theme,
-        "pr-3",
-        "pl-3",
-        "pt-2",
-        "pb-2",
-        display,
-        fontSize,
-        fontWeight,
-        textAlign,
-        borderWidth,
-        marginBottom,
-        marginLeft,
-        marginRight,
-        marginTop
-      )}
-      disabled={disabled}
-      {...props}
+    <BaseButton
+      //기본 패딩
+      paddingBottom="pb-3"
+      paddingTop="pt-3"
+      paddingRight="pr-4"
+      paddingLeft="pl-4"
+      // 사용자가 지정할 수 있는 것 (props로 받아옴)
+      fontSize={fontSize}
+      display={display}
+      borderColor={`border-${theme}`}
+      marginBottom={marginBottom}
+      marginLeft={marginLeft}
+      marginRight={marginRight}
+      marginTop={marginTop}
+      // white인 경우 font color는 검정색이여야 보이니까 지정
+      fontColor={theme === "white" ? `text-black` : `text-${theme}`}
+      borderWidth={borderWidth}
+      {...otherProps}
     >
       {icon}
       {children}
-    </button>
+    </BaseButton>
   );
 }
 
