f2348db379c3612708f9460a7620f21ce6871a08
JaeilJO <whwodlf97@gmail.com>
2024-03-09 01:40:09 +0900

    refactor : BaseButton을 중심으로 SolidButton 복잡도개선

--- src/components/Atoms/Button/SolidButton/index.stories.tsx
+++ src/components/Atoms/Button/SolidButton/index.stories.tsx
@@ -1,7 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
 import SolidButton from ".";
-import { BsGoogle } from "react-icons/bs";
 
 //👇 This default export determines where your story goes in the story list
 const meta: Meta<typeof SolidButton> = {
@@ -20,7 +19,7 @@ export const Custom: Story = {
 
 export const Theme: Story = {
   render: () => (
-    <div style={{ display: "flex", gap: "24px" }}>
+    <div>
       <SolidButton theme="primary">Primary</SolidButton>
       <SolidButton theme="gray">Gray</SolidButton>
       <SolidButton theme="black">Black</SolidButton>
@@ -29,151 +28,6 @@ export const Theme: Story = {
       <SolidButton theme="yellow">Yellow</SolidButton>
       <SolidButton theme="green">Green</SolidButton>
       <SolidButton theme="white">White</SolidButton>
-    </div>
-  ),
-};
-
-export const Display: Story = {
-  render: () => (
-    <div>
-      <SolidButton display="block" theme="primary">
-        Blcok
-      </SolidButton>
-      <SolidButton display="inline" theme="gray">
-        Inline
-      </SolidButton>
-    </div>
-  ),
-};
-
-export const Icon: Story = {
-  render: () => (
-    <div>
-      <SolidButton theme="primary" icon={<BsGoogle />}>
-        Google
-      </SolidButton>
-    </div>
-  ),
-};
-
-export const FontSize: Story = {
-  render: () => {
-    return (
-      <div>
-        <SolidButton
-          display="inline"
-          fontSize="tit-01"
-          theme="primary"
-          marginLeft="ml-3"
-        >
-          tit-01
-        </SolidButton>
-        <SolidButton
-          display="inline"
-          fontSize="tit-02"
-          theme="primary"
-          marginLeft="ml-3"
-        >
-          tit-02
-        </SolidButton>
-        <SolidButton
-          display="inline"
-          fontSize="tit-03"
-          theme="primary"
-          marginLeft="ml-3"
-        >
-          tit-03
-        </SolidButton>
-        <SolidButton
-          display="inline"
-          fontSize="sub-01"
-          theme="primary"
-          marginLeft="ml-3"
-        >
-          sub-01
-        </SolidButton>
-        <SolidButton
-          display="inline"
-          fontSize="sub-02"
-          theme="primary"
-          marginLeft="ml-3"
-        >
-          sub-02
-        </SolidButton>
-        <SolidButton
-          display="inline"
-          fontSize="body-01"
-          theme="primary"
-          marginLeft="ml-3"
-        >
-          body-01
-        </SolidButton>
-        <SolidButton
-          display="inline"
-          fontSize="body-02"
-          theme="primary"
-          marginLeft="ml-3"
-        >
-          body-02
-        </SolidButton>
-      </div>
-    );
-  },
-};
-
-export const FontWeight: Story = {
-  render: () => (
-    <div>
-      <SolidButton
-        display="inline"
-        marginLeft="ml-3"
-        fontWeight="font-bold"
-        theme="primary"
-      >
-        Bold
-      </SolidButton>
-      <SolidButton
-        display="inline"
-        marginLeft="ml-3"
-        fontWeight="font-light"
-        theme="primary"
-      >
-        Light
-      </SolidButton>
-      <SolidButton
-        display="inline"
-        marginLeft="ml-3"
-        fontWeight="font-regular"
-        theme="primary"
-      >
-        Regular
-      </SolidButton>
-    </div>
-  ),
-};
-
-export const TextAlign: Story = {
-  render: () => (
-    <div>
-      <SolidButton marginBottom="mb-3" textAlign="text-left" theme="primary">
-        Left
-      </SolidButton>
-      <SolidButton marginBottom="mb-3" textAlign="text-center" theme="primary">
-        Center
-      </SolidButton>
-      <SolidButton marginBottom="mb-3" textAlign="text-right" theme="primary">
-        Right
-      </SolidButton>
-    </div>
-  ),
-};
-
-export const Disabled: Story = {
-  render: () => (
-    <div>
-      <SolidButton disabled theme="primary">
-        Disabled Button
-      </SolidButton>
     </div>
   ),
 };
--- src/components/Atoms/Button/SolidButton/index.tsx
+++ src/components/Atoms/Button/SolidButton/index.tsx
@@ -1,50 +1,70 @@
-import classNames from "classnames/bind";
-import style from "./index.module.scss";
+import BaseButton, { buttonColorTheme } from "../BaseButton";
+import { BaseButtonProps } from "../BaseButton/index.type";
 
-import SolidButtonProps from "./index.type";
+// 사용자가 지정하면 안되는 부분 지정
+interface SolidButtonProps
+  extends Omit<
+    BaseButtonProps,
+    | "fontWeight"
+    | "backgroundColor"
+    | "borderColor"
+    | "fontColor"
+    | "paddingBottom"
+    | "paddingLeft"
+    | "paddingRight"
+    | "paddingTop"
+    | "borderWidth"
+  > {
+  //현재 버튼이 가지고 있는 고유 타입
+  theme: buttonColorTheme;
+}
 
-const cn = classNames.bind(style);
-function SolidButton({
-  theme,
-  display = "block",
-  children,
-  icon,
-  fontSize,
-  fontWeight,
-  textAlign = "text-center",
-  marginBottom,
-  marginTop,
-  marginLeft,
-  marginRight,
-  disabled,
-  type = "button",
-  ...props
-}: SolidButtonProps) {
+function SolidButton(props: SolidButtonProps) {
+  const {
+    theme = "primary",
+    display = "block",
+
+    fontSize = "body-01",
+
+    marginBottom,
+    marginRight,
+    marginTop,
+    marginLeft,
+
+    textAlign = "text-center",
+
+    icon,
+
+    disabled,
+
+    children,
+    ...otherProps
+  } = props;
+
   return (
-    <button
+    <BaseButton
+      //기본 패딩
+      paddingBottom="pb-3"
+      paddingTop="pt-3"
+      paddingRight="pr-4"
+      paddingLeft="pl-4"
+      // 사용자가 지정할 수 있는 것 (props로 받아옴)
+      fontSize={fontSize}
+      display={display}
+      textAlign={textAlign}
+      backgroundColor={`bg-${theme}`}
+      marginBottom={marginBottom}
+      marginLeft={marginLeft}
+      marginRight={marginRight}
+      marginTop={marginTop}
+      // white인 경우 빼고 모두 흰색
+      fontColor={theme === "white" ? `text-black` : `text-white`}
       disabled={disabled}
-      type={type}
-      className={cn(
-        "button",
-        theme,
-        "pr-3",
-        "pl-3",
-        "pt-2",
-        "pb-2",
-        display,
-        fontSize,
-        fontWeight,
-        textAlign,
-        marginBottom,
-        marginLeft,
-        marginTop,
-        marginRight
-      )}
-      {...props}
+      {...otherProps}
     >
       {icon}
       {children}
-    </button>
+    </BaseButton>
   );
 }
 
