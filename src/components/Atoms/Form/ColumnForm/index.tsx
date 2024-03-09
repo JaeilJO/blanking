f5845ec84c18afdc8719fe6dac440a82f1d03214
JaeilJO <whwodlf97@gmail.com>
2024-03-09 20:46:26 +0900

    refactor : Form에 forwardRef추가
    -AuthForm에서 Form에 reset을 해주는 기능이 있기때문에 ref를 넣기 위해 추가

--- src/components/Atoms/Form/ColumnForm/index.tsx
+++ src/components/Atoms/Form/ColumnForm/index.tsx
@@ -1,46 +1,53 @@
 import style from "./index.module.scss";
 import classNames from "classnames/bind";
 import ColoumFormProps from "./index.type";
+import { LegacyRef, forwardRef } from "react";
 
 const cn = classNames.bind(style);
 
-function ColumnForm({
-  children,
-  gap = "gap-0",
-  display = "block",
-  alignItems,
-  justifyContent = "flex-start",
-  marginBottom,
-  marginTop,
-  marginLeft,
-  marginRight,
-  paddingBottom,
-  paddingTop,
-  paddingLeft,
-  paddingRight,
-  ...props
-}: ColoumFormProps) {
-  const formClass = cn(
-    "form",
-    gap,
-    marginBottom,
-    marginTop,
-    marginLeft,
-    marginRight,
-    paddingBottom,
-    paddingTop,
-    paddingLeft,
-    paddingRight
-  );
-  return (
-    <form
-      className={formClass}
-      style={{ justifyContent, alignItems }}
-      {...props}
-    >
-      {children}
-    </form>
-  );
-}
+const ColumnForm = forwardRef(
+  (
+    {
+      children,
+      gap = "gap-0",
+      display = "block",
+      alignItems,
+      justifyContent = "flex-start",
+      marginBottom,
+      marginTop,
+      marginLeft,
+      marginRight,
+      paddingBottom,
+      paddingTop,
+      paddingLeft,
+      paddingRight,
+      ...props
+    }: ColoumFormProps,
+    ref
+  ) => {
+    const formClass = cn(
+      "form",
+      gap,
+      marginBottom,
+      marginTop,
+      marginLeft,
+      marginRight,
+      paddingBottom,
+      paddingTop,
+      paddingLeft,
+      paddingRight
+    );
+    return (
+      <form
+        ref={ref as LegacyRef<HTMLFormElement>}
+        className={formClass}
+        style={{ justifyContent, alignItems }}
+        {...props}
+      >
+        {children}
+      </form>
+    );
+  }
+);
 
 export default ColumnForm;
--- src/components/Atoms/Form/RowForm/index.tsx
+++ src/components/Atoms/Form/RowForm/index.tsx
@@ -1,47 +1,55 @@
 import style from "./index.module.scss";
 import classNames from "classnames/bind";
 import RowFormProps from "./index.type";
+import { ForwardedRef, forwardRef } from "react";
 
 const cn = classNames.bind(style);
 
-function RowForm({
-  children,
-  gap = "gap-0",
-  display = "block",
-  justifyContent = "flex-start",
-  alignItems,
-  marginBottom,
-  marginTop,
-  marginLeft,
-  marginRight,
-  paddingBottom,
-  paddingTop,
-  paddingLeft,
-  paddingRight,
-  ...props
-}: RowFormProps) {
-  const formClass = cn(
-    "form",
-    gap,
-    display,
-    marginBottom,
-    marginTop,
-    marginLeft,
-    marginRight,
-    paddingBottom,
-    paddingTop,
-    paddingLeft,
-    paddingRight
-  );
-  return (
-    <form
-      className={formClass}
-      style={{ justifyContent, alignItems }}
-      {...props}
-    >
-      {children}
-    </form>
-  );
-}
+const RowForm = forwardRef(
+  (
+    {
+      children,
+      gap = "gap-0",
+      display = "block",
+      justifyContent = "flex-start",
+      alignItems,
+      marginBottom,
+      marginTop,
+      marginLeft,
+      marginRight,
+      paddingBottom,
+      paddingTop,
+      paddingLeft,
+      paddingRight,
+
+      ...props
+    }: RowFormProps,
+    ref: ForwardedRef<HTMLFormElement>
+  ) => {
+    const formClass = cn(
+      "form",
+      gap,
+      display,
+      marginBottom,
+      marginTop,
+      marginLeft,
+      marginRight,
+      paddingBottom,
+      paddingTop,
+      paddingLeft,
+      paddingRight
+    );
+    return (
+      <form
+        ref={ref}
+        className={formClass}
+        style={{ justifyContent, alignItems }}
+        {...props}
+      >
+        {children}
+      </form>
+    );
+  }
+);
 
 export default RowForm;
