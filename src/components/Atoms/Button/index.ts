ba197aea872715ccb8a722b33b12b72ce4a5d649
JaeilJO <whwodlf97@gmail.com>
2024-03-10 22:27:32 +0900

    refactor : Button에 IndicatorButton과 SquareButton추가

--- src/components/Atoms/Button/index.ts
+++ src/components/Atoms/Button/index.ts
@@ -1,11 +1,13 @@
 import LineButton from "./LineButton";
 import SolidButton from "./SolidButton";
+import SquareButton from "./SquareButton";
 import TextButton from "./TextButton";
 
 const Button = {
   Line: LineButton,
   Solid: SolidButton,
   Text: TextButton,
+  Square: SquareButton,
 };
 
 export default Button;
