1af8062119feb3a3d58225e04a7ef6e7d99ca757
JaeilJO <whwodlf97@gmail.com>
2024-03-13 00:06:19 +0900

    feat : Social Sign In Buttons 생성

---
+++ src/components/Organisms/SocialSignInButtons/index.tsx
@@ -0,0 +1,20 @@
+import FlexBox from "@/components/Atoms/FlexBox";
+import SocialSignInButton from "@/components/Molecules/SocialSignInButton";
+import { SocialType } from "@/utils/socials";
+
+function SocialSignInButtons({ socials }: { socials: SocialType[] }) {
+  return (
+    <FlexBox.Row gap="gap-3" display="inline-flex">
+      {socials.map((social) => (
+        <SocialSignInButton
+          key={social.socialName}
+          socialName={social.socialName}
+          icon={social.icon}
+          color={social.color}
+        />
+      ))}
+    </FlexBox.Row>
+  );
+}
+
+export default SocialSignInButtons;
