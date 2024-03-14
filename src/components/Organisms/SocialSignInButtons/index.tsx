dd85e110ee74f2cff147ae6fdfecbb662bf19b3b
JaeilJO <whwodlf97@gmail.com>
2024-03-15 02:28:58 +0900

    fix : 사용되지 않은 옵션값을 수정

--- src/components/Organisms/SocialSignInButtons/index.tsx
+++ src/components/Organisms/SocialSignInButtons/index.tsx
@@ -4,7 +4,7 @@ import { SocialType } from "@/utils/socials";
 
 function SocialSignInButtons({ socials }: { socials: SocialType[] }) {
   return (
-    <FlexBox.Row gap="gap-3" display="inline-flex">
+    <FlexBox.Row gap="gap-3" display="inline">
       {socials.map((social) => (
         <SocialSignInButton
           key={social.socialName}
