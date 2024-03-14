098532dbfa541ecf6008338fdeeb851b8ff742cc
JaeilJO <whwodlf97@gmail.com>
2024-03-14 10:32:14 +0900

    feat : Main Wrapper 추가

--- src/components/Atoms/Wrappers/MainWrapper/index.tsx
+++ src/components/Atoms/Wrappers/MainWrapper/index.tsx
@@ -1,5 +1,26 @@
-function MainWrapper() {
-  return <main></main>;
+"use client";
+
+import { useSideBarStatusStore } from "@/zustand/sideBarStatusStore";
+import style from "./index.module.scss";
+import classNames from "classnames/bind";
+import { TiChevronLeft } from "react-icons/ti";
+
+const cn = classNames.bind(style);
+function MainWrapper({ children }: { children: React.ReactNode }) {
+  const { status, sidebarStatusHandler } = useSideBarStatusStore(
+    (state) => state
+  );
+  return (
+    <main className={cn("main", { close: !status })}>
+      {children}
+      <button
+        onClick={sidebarStatusHandler}
+        className={cn("button", { close: !status })}
+      >
+        <TiChevronLeft />
+      </button>
+    </main>
+  );
 }
 
 export default MainWrapper;
