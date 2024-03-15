01d4a58611e138499c30a3a30d27430dea6a92e5
JaeilJo <whwodlf97@gmail.com>
2024-03-16 02:26:28 +0900

    refactor : Square Link useEffect 분리

--- src/components/Atoms/Link/SquareLink/index.tsx
+++ src/components/Atoms/Link/SquareLink/index.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useEffect, useRef } from "react";
 import style from "./index.module.scss";
 
 import classNames from "classnames/bind";
@@ -8,72 +7,59 @@ import classNames from "classnames/bind";
 import Link from "next/link";
 import SquareLinkProps from "./index.type";
 
+import uselinkEventListeners from "./index.hook";
+
 const cn = classNames.bind(style);
 
+// 이 색상은 기본 검정 색상입니다.
+const blackColor = "#333333";
+
 function SquareLink(props: SquareLinkProps) {
-  const buttonRef = useRef<HTMLAnchorElement>(null);
-
   const {
-    children,
+    size = 30,
 
-    // 이 색상은 기본 검정 색상입니다.
-    color = "#333333",
+    color = blackColor,
 
     fontSize = "body-01",
-
-    size = 30,
 
     marginLeft,
     marginRight,
     marginTop,
     marginBottom,
+
     href,
+
+    disabled,
+
+    children,
+
+    style,
   } = props;
 
   const linkClassName = cn(
     "link",
+
     fontSize,
 
     marginLeft,
     marginRight,
     marginTop,
-    marginBottom
+    marginBottom,
+
+    { disabled: disabled }
   );
 
-  useEffect(() => {
-    const $button = buttonRef.current;
-
-    if (!$button) return;
-
-    const handleMouseEnter = () => {
-      $button.style.borderColor = color;
-      $button.style.borderStyle = "solid";
-      $button.style.borderWidth = "1px";
-    };
-
-    const handleMouseLeave = () => {
-      //light-gray 기본컬러입니다.
-      $button.style.borderColor = "#e2e2e2";
-      $button.style.borderStyle = "solid";
-      $button.style.borderWidth = "1px";
-    };
-
-    $button.addEventListener("mouseenter", handleMouseEnter);
-    $button.addEventListener("mouseleave", handleMouseLeave);
-    return () => {
-      $button.removeEventListener("mouseenter", handleMouseLeave);
-      $button.removeEventListener("mouseleave", handleMouseLeave);
-    };
-  }, [color]);
+  const { linkRef } = uselinkEventListeners({ color });
 
   return (
     <Link
-      ref={buttonRef}
+      ref={linkRef}
       href={href}
       style={{
         color,
         width: `${size}px`,
         height: `${size}px`,
+        ...style,
       }}
       className={linkClassName}
     >
