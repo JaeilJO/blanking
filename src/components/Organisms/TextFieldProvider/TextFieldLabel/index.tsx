0b95513367af829c72b925b0ec14021d6230bdd6
JaeilJo <whwodlf97@gmail.com>
2024-03-16 21:58:08 +0900

    refactor : Lebel로 가려진 input이 클릭이 안되는 문제 해결

--- src/components/Organisms/TextFieldProvider/TextFieldLabel/index.tsx
+++ src/components/Organisms/TextFieldProvider/TextFieldLabel/index.tsx
@@ -12,14 +12,13 @@ const cn = classNames.bind(style);
 
 function TextFieldLabel() {
   const value = useContext(TextFieldContext);
-  const { label, placeholder, isFocus, theme, disabled } = value!;
+  const { label, placeholder, isFocus, theme } = value!;
 
   // Label이 없을 경우 생성되지 않음
   if (!label) return null;
 
   const labelClassName = cn("basic", `text-${theme}`, {
     focus: isFocus,
-    disabled: disabled,
   });
 
   return <label className={labelClassName}>{placeholder}</label>;
