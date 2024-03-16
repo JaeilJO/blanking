452ad1f1a82596129165ddbc216e21b89eb458b5
JaeilJo <whwodlf97@gmail.com>
2024-03-16 22:01:33 +0900

    delete : InputWithLabel 제거

--- src/components/Atoms/Input/index.ts
+++
@@ -1,9 +0,0 @@
-import PasswordInput from "./PasswordInput";
-import TextInput from "./TextInput";
-
-const Input = {
-  Text: TextInput,
-  Password: PasswordInput,
-};
-
-export default Input;
--- src/components/Molecules/InputWithLabel/index.tsx
+++
@@ -1,9 +0,0 @@
-import InputWithLabelPasswordInput from "./InputWithLabelPasswordInput";
-import InputWithLabelTextInput from "./InputWithLabelTextInput";
-
-const InputWithLabel = {
-  Text: InputWithLabelTextInput,
-  Password: InputWithLabelPasswordInput,
-};
-
-export default InputWithLabel;
